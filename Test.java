8bac6347f6395a32dbd8f8b0da4b4708f08d2e32
Rhymart P. Manchus <rhymart.manchus@gmail.com>
2013-06-13 14:53:34 +0800

    asdfasdf

--- Test.java
+++ Test.java
@@ -4,14 +4,10 @@ double i = Double.MAX_VALUE;
 System.out.println("Double.ANDROID MAX_VALUE => "+Double.MAX_VALUE);
 System.out.println("Double.MIN_VALUE => "+Double.MIN_VALUE);
 System.out.println("Max DOUBLE => "+i++);
-<<<<<<< HEAD
 System.out.println("BEYOND Max DOUBLE? => "+i+"aksjdhakshjd lkj");
 
 }
 asdasdas
-=======
 System.out.println("BEYOND Max DOUBLE? => "+i);asdasdasd
-asdasd
-}asdasd
->>>>>>> c76d67114a2747beb3bd110bf1bdf5881ae635bf
+asdasdssssssss;oeirytp9 qe98tp98
 }
