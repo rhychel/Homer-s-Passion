c76d67114a2747beb3bd110bf1bdf5881ae635bf
rhychel <rhymart.manchus@gmail.com>
2013-06-13 15:51:07 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ double i = Double.MAX_VALUE;
 System.out.println("Double.MAX_VALUE => "+Double.MAX_VALUE);
 System.out.println("Double.MIN_VALUE => "+Double.MIN_VALUE);
 System.out.println("Max DOUBLE => "+i++);
-System.out.println("BEYOND Max DOUBLE? => "+i);
-
-}
+System.out.println("BEYOND Max DOUBLE? => "+i);asdasdasd
+asdasd
+}asdasd
 }
