2063d07c5638eee42a99bfc734c10121c4b87556
tramyhr <tramyhr_LV51008@yahoo.com>
2013-06-13 14:45:27 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,6 +5,14 @@ System.out.println("Double.MAX_VALUE => "+Double.MAX_VALUE);
 System.out.println("Double.MIN_VALUE => "+Double.MIN_VALUE);
 System.out.println("Max DOUBLE => "+i++);
 System.out.println("BEYOND Max DOUBLE? => "+i);
+as
+a
+sd
+
+
+as
+d
+
 
 }
 }
